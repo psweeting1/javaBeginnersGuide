36d10a5e85f0387588991d3685b9c39240943a1b
Patrick Sweeting <patrick.sweeting@landregistry.gov.uk>
2020-01-07 14:05:29 +0000

    first java class

---
+++ Java Fundamentals/First Simple Program/src/Example.java
@@ -0,0 +1,12 @@
+/*
+ * This is a simple Java Program
+ *
+ * Call this file Example.java
+ *
+ * */
+
+public class Example {
+    public static void main (String arg[]) {
+     System.out.println("Java drives the Web");
+    }
+}
