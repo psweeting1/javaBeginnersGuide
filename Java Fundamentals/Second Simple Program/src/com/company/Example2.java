56e454adcc8ff3879d845f7f03bc551738f00ff4
Patrick Sweeting <patrick.sweeting@landregistry.gov.uk>
2020-01-07 14:55:10 +0000

    Second simple program

---
+++ Java Fundamentals/Second Simple Program/src/com/company/Example2.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int myVar1; // This declares a variable
+        int myVar2;
+
+        myVar1 = 1024; // Assign 1024 to myVar1
+
+        System.out.println("My var1 contains " + myVar1);
+
+        myVar2 = myVar1 / 2;
+
+        System.out.println("My var2 contains myVar1 / 2: " + myVar2);
+        System.out.println(myVar2);
+    }
+}
